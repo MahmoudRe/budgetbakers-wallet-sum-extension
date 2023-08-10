5ee9a0f0e3ab7848e40f21704918f54bded6702a
Mahmoud <m.alhumsialrefaai@student.tudelft.nl>
2023-08-10 14:11:04 +0800

    Show the sum per currency

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Budgetbakers Wallet sum-extension
 
-At the time of development, the web-base version of Wallet doesn't show the sum of records in foreign currency, rather it convert them to the default currency of the account.
+At the time of development, the web-base version of Wallet doesn't show the sum of records per currency or in forging currency, rather it convert all currency to the default one of the account, then it shows the sum of that.
 
-Currently, this ~~extension~~ hack sum all the records that are rendered. It sum the numbers regardless of the currency, hence it is useful for records of the same (foreign) currency. It is a quick-hack rather a full extension.
-
-A listener to re-calculate the sum fires on `scrollend``, so after scrolling down to ensure all records are rendered it shows the correct results next to the sum of the dashboard.
+Currently, this ~~extension~~ quick-hack sum all the records that are rendered. A listener to re-calculate the sum fires on `click` event on filter side-baron and on `scrollend`, so scroll down to ensure all records are rendered and the correct results is shown next to the total sum of the dashboard.
--- index.js
+++ index.js
@@ -2,8 +2,15 @@ function renderSumEl() {
     let sum = [...document.querySelector("#root > div > div > main > div > div:last-child > div:last-child > div").children]
         .slice(1, -1).map(e =>[...e.children].slice(1)).flat(1)
         .map(e => [...e.lastChild.children].slice(-2, -1)).flat(1)
-        .map(e => parseFloat(e.firstChild.textContent.match(/[\.\d\-]/g).join("")))
-        .reduce((acc, e) => acc + e, 0).toFixed(2);
+        .reduce((acc, e) => {
+            let amount = parseFloat(e.firstChild.textContent.match(/[\.\d\-]/g).join(""))
+            let currency = e.firstChild.textContent.match(/[^\d\.\-\ \ \,]/g).join("")
+
+            if(!acc[currency]) acc[currency] = 0
+            acc[currency] += amount
+            
+            return acc
+        }, {})
     
     if(!document.querySelector(".custom-sum-element")) {
         let sumEl =  document.createElement('span')
@@ -11,7 +18,11 @@ function renderSumEl() {
         document.querySelector("#root > div > div > main > div > div:last-child > div:last-child > div").firstChild.lastChild.firstChild.prepend(sumEl)
     }
 
-    document.querySelector(".custom-sum-element").textContent = sum
+    let res = Object.entries(sum)
+        .map(([key, val]) => "<span>" + key + " " + val.toFixed(2) + "</span>")
+        .join("")  // &#160; = space
+
+    document.querySelector(".custom-sum-element").innerHTML = res
 }
 
 let trial = 0;
@@ -22,7 +33,8 @@ function tryRender() {
     setTimeout(() => {
         try {
             renderSumEl()
-            document.querySelector("#root > div > div > main > div > div:last-child > div:first-child").addEventListener('click', renderSumEl)
+            document.querySelector("#root > div > div > main > div > div:last-child > div:first-child")
+                .addEventListener('click', () => setTimeout(renderSumEl, 200))
             document.addEventListener("scrollend", renderSumEl);
         } catch(e) {
             tryRender()
